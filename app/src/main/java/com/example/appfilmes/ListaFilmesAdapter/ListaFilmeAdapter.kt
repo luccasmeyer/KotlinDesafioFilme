733780d8f67262beab5ccbe281be7683ff0a9d47
luccasmeyer <81263064+luccasmeyer@users.noreply.github.com>
2022-09-09 16:30:43 -0300

    Criação e configuração do Adapter

--- app/src/main/java/com/example/appfilmes/ListaFilmesAdapter/ListaFilmeAdapter.kt
+++ app/src/main/java/com/example/appfilmes/ListaFilmesAdapter/ListaFilmeAdapter.kt
@@ -1,21 +1,47 @@
 package com.example.appfilmes.ListaFilmesAdapter
 
-import android.content.Context
-import android.view.View
-import android.widget.Adapter
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.appfilmes.ObjetosFilmes.Filme
-import com.example.appfilmes.databinding.ActivityMainBinding
+import com.example.appfilmes.databinding.FilmeItemBinding
 
-class ListaFilmeAdapter (private val filmes: List<Filme>, private val context: Context) :
-Adapter<ListaFilmeAdapter.ViewHolder>() {
+class ListaFilmeAdapter: RecyclerView.Adapter<ListaFilmeAdapter.ViewHolder>(){
 
-    class ViewHolder(
-        view: View,
-        private val binding: ListaFilmeItemBinding
-    ): RecyclerView.ViewHolder(view)
-    fun vincula(filme: Filme){
-        Glide.with(Filme).this()
+//root pega o "item pai"
+
+    val listaFilmeAdapter: ArrayList<Filme> = arrayListOf()
+
+    inner class ViewHolder(private val binding: FilmeItemBinding): RecyclerView.ViewHolder(binding.root){
+
+        fun bind(banana: Filme){
+            binding.notaItemTitulo.text = banana.titulo
+            binding.dataDeLancamento.text = banana.dataLancamento
+            binding.linguagem.text = banana.linguagem
+
+            val imageurl = "https://image.tmdb.org/t/p/w500${banana.imagemVertical}"
+            Glide.with(binding.root).load(imageurl).into(binding.filmeImagem)
+
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(FilmeItemBinding.inflate(LayoutInflater.from(parent.context),parent, false))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(listaFilmeAdapter[position])//atualiza a posição do adapter
+    }
+
+    override fun getItemCount(): Int {
+        return listaFilmeAdapter.size
+    }
+
+    fun populaAdapter(listaFilme: List<Filme>){
+        val oldrangeitens = listaFilmeAdapter.size
+        val newrangeitens = listaFilme.size
+        notifyItemRangeInserted(oldrangeitens, newrangeitens)
+        //chama sempre novos 20 itens
     }
 }
